e4f05989dc7844a248980354238035cab0b9e6f9
Andrew Theiss <andrew.theiss@gmail.com>
2021-04-11 11:02:54 -0700

    2--CalculateErrorByCheckingAgainstTraining

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+#Here we are going to do two steps
+# 1 - Calculate the Output
+# 2 - Compare the output to the expected Output
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def sigmoid_derrivative(x):
+    return x * (1 - x)
 
 training_inputs = np.array([
     [0,0,1],
@@ -12,7 +19,7 @@ training_inputs = np.array([
 
 # .T Transpose the matrix so its a 1 x 4
 training_outputs = np.array(
-    [0,1,1,0]
+    [[0,1,1,0]]
 ).T
 
 np.random.seed(1)
@@ -34,6 +41,14 @@ for iteration in range(1):
 
     # Squish our result between 0 and 1 by using our normalizing fn
     normalized_outputs = sigmoid(unsquished_outputs)
+    print('Normalized Outputs')
+    print(normalized_outputs)
 
-print('Outputs after training')
-print(normalized_outputs)
+    # 2 - Calc error by checking the training outputs with our calulated ones
+    error = training_outputs - normalized_outputs
+    print('Error')
+    print(error)
+
+    adjustments = error * sigmoid_derrivative(normalized_outputs)
+    print('Adjustments')
+    print(adjustments)
